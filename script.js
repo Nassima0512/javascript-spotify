1ae468a54a983ba95c379545ac72b026cc5f1467
smarieantoine <smarieantoine@gmail.com>
2021-06-01 08:32:18 +0200

    V2 ajout data attributes

--- script.js
+++ script.js
@@ -1,12 +1,63 @@
 
-const ajouter = document.getElementsByTagName("button")[0];
-const buttons = document.querySelectorAll("button")
-//const supprimer =document.querySelector('#secondSection button')
+//const ajouter = document.getElementsByTagName("button")[0];
+
+//Cible des li de result
+const li1=document.getElementById("li1");
+const li2=document.getElementById("li2");
+const li3=document.getElementById("li3");
+const li4=document.getElementById("li4");
+const li5=document.getElementById("li5");
+
+// Lecture des valeurs de dataset
+
+const li1t=li1.dataset.text;
+const li2t=li2.dataset.text;
+const li3t=li3.dataset.text;
+const li4t=li4.dataset.text;
+const li5t=li5.dataset.text;
+
+//Controle des index de dataset et affectation
+
+function add(index){
+    if(index===1){
+        index=li1t;
+        addli(index);
+    }
+    if(index===2){
+        index=li2t;
+        addli(index);
+    }
+    if(index===3){
+        index=li3t;
+        addli(index);
+    }
+    if(index===4){
+        index=li4t;
+        addli(index);
+    }
+    if(index===5){
+        index=li5t;
+        addli(index);
+    }
+}
+
+function addli(index){
+    const newLi=document.createElement('li');
+    const btnRemove=document.createElement('button');
+    const textbtn = document.createTextNode("Supprimer");
+
+    btnRemove.append(textbtn)
+    newLi.append(index);
+    const ul1=document.querySelector("#secondSection li");
+    newLi.append(btnRemove)
+    ul1.append(newLi);
+
+    btnRemove.addEventListener("click", ()=>{
+        newLi.remove();
+    });
+}
 
 
-buttons.forEach(function (city) {
-	
-});
 
 /*ajouter.addEventListener('click' , () =>{
     
@@ -15,7 +66,6 @@ buttons.forEach(function (city) {
     const textbtn = document.createTextNode("Supprimer");
 
     btnRemove.append(textbtn)
-    
     newLi.append(document.getElementsByTagName('li')[0].textContent);
     const ul1=document.querySelector("#secondSection li");
     newLi.append(btnRemove)
@@ -25,31 +75,6 @@ buttons.forEach(function (city) {
         newLi.remove();
     })
 });*/
-
-function add(){
-    
-    const newLi=document.createElement('li');
-    const btnRemove=document.createElement('button');
-    const textbtn = document.createTextNode("Supprimer");
-
-    btnRemove.append(textbtn)
-    
-    newLi.append(document.getElementsByTagName('li')[0].textContent);
-    const ul1=document.querySelector("#secondSection li");
-    newLi.append(btnRemove)
-    ul1.append(newLi);
-
-    btnRemove.addEventListener("click", ()=>{
-        newLi.remove();
-    })
-    
-
-
-}
-
-
-
-
 
 
 
