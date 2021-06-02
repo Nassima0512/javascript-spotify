703864f903d558ecd8a442459e40f0b126ba0244
Nassima <nassima.chebbat@gmail.com>
2021-06-02 17:41:17 +0200

    v4 remplacer artist par track

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 
 const search=document.getElementById('search');
 const rechercher = document.getElementsByTagName("button")[0];
-
-rechercher.addEventListener('click', async() =>{
+const sauvegarder = document.querySelector("#user button");
+rechercher.addEventListener('click', () =>{
   const textSearch=search.value;
    //const userAccessToken="BQBR6VxQ-luy8jfT65f58X0j2UrbuHzQAFsXUU09wu5mngYXyngRu0fY_-rgzlcbGMBeAOhLptwLy4FZ8HOJ9pQXE3qqiYQIryhwpDs5ibyatAedSWENsKZS8WMa9LIr_fG-ClMDQLtRTi4yvVd-wOCbWfSjTfxvxf6VpAR6SwcyHSP9zA";
-   const userAccessToken="BQAOMFN7vQiSi7kO7yWaz8oyTSC__-nMXSnwG9VJqKe4BGkktAApabRzZL8VHCahBMkGNTwzaDSEhmWR1M7U3ofAvcLygV1Mn6LFH-u65XElU0-tmFKLNnPZktI5JuDZ6E1ZRTXQMaIxY6Yf29ZLuoVH2tJx3pw";
+   const userAccessToken="BQCCAuSK7lLKl1BjjE_VZr88nXHjAkvKIOheaZNZ5Jwibb05YA7Cby0az2OYu16lfii6TAuKKMKrZwwnR_GimSq6u0RUB5sJH9R217PeqqyXpJqwY4BFNMwo6YDaMhF4yEbPOfvQQ23BPIpPdAnZ4yVbqspE0UrM_NbmueEYNwqMZe7CDw";
    const options={
      method: "GET",
      "Content-type": "application/json",
      headers: {
-       Authorization: `Bearer ${userAccessToken}`
+     Authorization: `Bearer ${userAccessToken}`
      }};
   
   //const url='https://api.spotify.com/v1/search?q=$search&type=artist';
-   const url=`https://api.spotify.com/v1/search?q=${textSearch}&type=artist`;
+   const url=`https://api.spotify.com/v1/search?q=${textSearch}&type=track`;
    fetch(url, options)
      .then(response => response.json())
      .then(data=>{ 
@@ -23,42 +23,45 @@ rechercher.addEventListener('click', async() =>{
       const newName = document.createElement('p');
       const newHref = document.createElement('p');
       const newUri = document.createElement('p');
-      newId.textContent = `${data.artists.items[0].id}`;
-			newName.textContent = `${data.artists.items[0].name}`; 
-			newHref.textContent = `${data.artists.items[0].href}`; 
-      newUri.textContent = `${data.artists.items[0].uri}`;
+      const newMarket = document.createElement('p');
+      
+      newId.textContent = `${data.tracks.items[0].id}`;
+			newName.textContent = `${data.tracks.items[0].name}`; 
+		  newHref.textContent = `${data.tracks.items[0].href}`; 
+      newUri.textContent = `${data.tracks.items[0].uri}`;
+      newMarket.textContent = `${data.tracks.items[0].available_markets}`;
 			const newImg = document.createElement('img');
-			newImg.style.width = "30px";
-			newImg.setAttribute("src", `${data.artists.items[0].images[0].url}`);
+			newImg.style.width = "40px";
+			newImg.setAttribute("src", `${data.tracks.items[0].album.images[0].url}`);
 			
-      addListe(newId,newName,newHref,newImg,newUri);
+      addListe(newId,newName,newImg,newUri,newMarket);
   });
 });
 
-function addListe(newId,newName,newHref,newImg,newUri){
+function addListe(newId,newName,newImg,newUri,newMarket){
   const newLi=document.createElement('li');
   const btnAdd=document.createElement('button');
   const textbtn = document.createTextNode("Ajouter");
 
   btnAdd.append(textbtn)
-  newLi.append(newId,newName,newHref,newImg,newUri);
+  newLi.append(newName,newImg);
   const ul1=document.querySelector("#firstSection ul");
   newLi.append(btnAdd)
   ul1.append(newLi);
   btnAdd.addEventListener("click", ()=>{
-    addPlaList(newId,newName,newHref,newImg,newUri);
+    addPlaList(newId,newName,newImg,newUri,newMarket);
     newLi.remove();
     textbtn.remove();
   });
 }
 
-function addPlaList(newId,newName,newHref,newImg,newUri){
+function addPlaList(newId,newName,newImg,newUri,newMarket){
   const newLi=document.createElement('li');
   const btnRemove=document.createElement('button');
   const textbtn = document.createTextNode("Supprimer");
 
   btnRemove.append(textbtn)
-  newLi.append(newId,newName,newHref,newImg,newUri);
+  newLi.append(newId,newName,newImg,newUri,newMarket);
   const ul1=document.querySelector("#secondSection ul");
   newLi.append(btnRemove)
   ul1.append(newLi);
@@ -68,4 +71,8 @@ function addPlaList(newId,newName,newHref,newImg,newUri){
     textbtn.remove();
   });
 }
+
+sauvegarder.addEventListener('click', ()=> {
+
+})
 
